4e9660c59d1aa7c583fde1fa060ce5f147aef5f5
leon <leon.yliu@gmail.com>
2015-02-02 23:34:19 -0500

    Add Drop example

---
+++ libGDX/desktop/src/com/lsmn/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.lsmn.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.lsmn.game.MyGdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGdxGame(), config);
+	}
+}
